addca9a73eb10c389d85cea9f17924f02e75f617
mojqva <majanka@i.ua>
2021-03-13 23:59:44 +0200

    Homework 3
    Я сделал, было очень интересно разбираться. Хотелось бы услышать любые замечания по коду

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,17 +1,17 @@
 import s from './style.module.css';
 
-const Layout = ({title, descr, urlBg, colorBg}) => {
-    const bg = urlBg ? {backgroundImage : `url(${urlBg})`} : {backgroundColor : colorBg};
+const Layout = ({title, urlBg, colorBg, children}) => {
+    if (!title) return null;
     return (
-        <section className = {s.root} style = {bg}>
+        <section className = {s.root} style = {{background: urlBg ? `url(${urlBg})` : colorBg}}>
             <div className = {s.wrapper}>
                 <article>
                     <div className = {s.title}>
                         <h3> {title}</h3>
                         <span className = {s.separator}></span>
                     </div>
-                    <div className = {`${s.desc}${s.full}`}>
-                        <p>{descr}</p>
+                    <div className = {`${s.desc} ${s.full}`}>
+                        {children}
                     </div>
                 </article>
             </div>
